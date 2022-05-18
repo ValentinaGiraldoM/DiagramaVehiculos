36e6d9762d786e1e1a55038ea1eb8f86e028db89
ynovalle67@misena.edu.co <>
2022-05-18 16:40:53 -0500

    Comienzo

---
+++ DiagramaVehiculos/SubClaseTerrestre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramaVehiculos
+{
+    internal class SubClaseTerrestre
+    {
+        private int cantidadLlantas;
+        private string nombreTerrestre;
+        private int nombrePersonas;
+
+        public SubClaseTerrestre
+            (int cantidadLlantas, int nombrePersonas,
+            string nombreTerrestre)
+        { 
+        this.cantidadLlantas = cantidadLlantas;
+        }
+    }
+}
---
+++ DiagramaVehiculos/SuperClaseVehiculos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramaVehiculos
+{
+    internal class SuperClaseVehiculos
+    {
+        private string nombreVehiculo;
+        private string tipoMotor;
+        private int cantidadPersonas;
+
+        public SuperClaseVehiculos(string nombreVehiculo, 
+            string tipoMotor, int cantidadPersonas)
+
+        {   this.nombreVehiculo = nombreVehiculo;
+            this.tipoMotor = tipoMotor;
+            this.cantidadPersonas = cantidadPersonas;
+        }
+
+
+        public string getNombreVehiculo()
+
+        { return nombreVehiculo; }
+        public void setNombreVehiculo(string nombreVehiculo)
+        { this.nombreVehiculo = nombreVehiculo; }
+
+
+        public string getTipoMotor()
+        { return tipoMotor; }
+        public void setTipoMotor(string tipoMotor)
+        { this.tipoMotor = tipoMotor; }
+
+        public int getCantidadPersonas()
+        { return cantidadPersonas; }
+        public void setCantidadPersonas(int cantidadPersonas) 
+        { this.cantidadPersonas = cantidadPersonas; }
+
+    }
+
+}
